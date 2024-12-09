f19df2c9a883f0c4532d979390c0862ff30ef022
Eliska Preclikova <eliska.preclikova.s@gyarab.cz>
2024-12-09 21:23:25 +0100

    funkcni pexeso

--- pexeso/pexeso.js
+++ pexeso/pexeso.js
@@ -1,32 +1,33 @@
 let pocetDvojic = 3
 let rubKarty = "heart.svg"
+let prazdneMisto = "prazdno.svg"
 let sirkaKarty = 100
 let vyskaKarty = 100
 
 let karty = [
     {
-        src:"https://placecats.com/neo_banana/100/100"
+        src: "https://placecats.com/neo_banana/100/100"
     },
     {
-        src:"https://placecats.com/neo_2/100/100" 
+        src: "https://placecats.com/neo_2/100/100"
     },
     {
-        src:"https://placecats.com/millie_neo/100/100"
+        src: "https://placecats.com/millie_neo/100/100"
     },
     {
-        src:"https://placecats.com/millie_neo/100/100"
+        src: "https://placecats.com/millie_neo/100/100"
     },
     {
-        src:"https://placecats.com/millie_neo/100/100"
+        src: "https://placecats.com/millie_neo/100/100"
     },
     {
-        src:"https://placecats.com/millie_neo/100/100"
+        src: "https://placecats.com/millie_neo/100/100"
     },
     {
-        src:"https://placecats.com/millie_neo/100/100"
+        src: "https://placecats.com/millie_neo/100/100"
     },
     {
-        src:"https://placecats.com/millie_neo/100/100"
+        src: "https://placecats.com/millie_neo/100/100"
     }
 
 ]
@@ -41,55 +42,80 @@ function zamichejPole(array) {
     return array;
 }
 
-function zalozKartu (i)
-{
-  const element = document.createElement('img')
-  const karta = {karta:i, el: element }  
-  element.width = sirkaKarty
-  element.height = vyskaKarty
-  element.src = rubKarty
-  element.onclick = () => otocKartu (karta)
-  return karta
+function zalozKartu(i) {
+    const element = document.createElement('img')
+    const karta = { karta: i, el: element }
+    element.width = sirkaKarty
+    element.height = vyskaKarty
+    element.src = rubKarty
+    element.onclick = () => otocKartu(karta)
+    return karta
 }
 
-function otocKartu (karta)
-{
-  if (otocene.length >= 2)
-  {
-    otocene.forEach((karta) => {
-        karta.el.src = rubKarty
+function otocKartu(karta) {
+    console.log(otocene)
+    console.log(nalezene)
+
+    if (otocene.length >= 2) {
+        otocene.forEach((karta) => {
+            karta.el.src = rubKarty
+        })
+        otocene = []
+        return
+    }
+
+    if (!otocene.includes(karta) && !nalezene.includes(karta)) {
+        karta.el.src = karty[karta.karta].src
+        otocene.push(karta)
+        kontrolaBodu()
+    }
+}
+function kontrolaBodu() {
+    if (otocene.length == 2 && otocene[1].karta == otocene[0].karta) {
+        nalezene = nalezene.concat(otocene)
+        otocene.forEach((karta) => {
+            setInterval(() => { karta.el.src = prazdneMisto }, 2000)
+
     })
     otocene = []
-    return
-  }
+    score.innerHTML = parseInt(score.innerHTML) + 1
+}
+}
 
-  if (!otocene.includes (karta))
-  {
-    karta.el.src = karty[karta.karta].src
-    otocene.push(karta)
-    kontrolaBodu ()
-  }  
-}
-function kontrolaBodu(){
-    if (otocene.length==2 && otocene[1].karta==otocene[0].karta)
-    {
-        otocene = []
-    }  
-}
 let otocene = []
+let nalezene = []
 let pole = []
-for (let i = 0; i < pocetDvojic; i++) {
-    pole.push(zalozKartu (i));
-    pole.push(zalozKartu (i));
+
+function novaHra() {
+    otocene = []
+    nalezene = []
+    pole = []
+
+    pocetDvojic = document.getElementById('pocetDvojic').value
+    pocetKaretNaRadek = document.getElementById('pocetKaretNaRadek').value
+
+    for (let i = 0; i < pocetDvojic; i++) {
+        pole.push(zalozKartu(i));
+        pole.push(zalozKartu(i));
+    }
+
+    zamichejPole(pole)
+
+    el = document.getElementById('pexeso')
+    el.innerHTML = "Skore"
+    score = document.createElement('div')
+    score.innerHTML = "0"
+    el.appendChild(score)
+    let i = 0
+    pole.forEach((karta) => {
+
+        el.appendChild(karta.el)
+        if (++i % pocetKaretNaRadek == 0) {
+            el.appendChild(document.createElement('br'))
+        }
+
+    })
 }
 
-zamichejPole (pole)
-
-el = document.getElementById('pexeso')
-
-pole.forEach((karta) => {
-    
-    el.appendChild(karta.el)
-    // el.innerHTML += karty[prvek].src + "<br>"
-})
+novaHra()
 console.log('END')
